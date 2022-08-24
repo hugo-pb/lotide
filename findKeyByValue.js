71e9b2616234caa5c61ba49ac7a0c088320d57bd
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-24 15:41:50 -0600

    test cases added

---
+++ findKeyByValue.js
@@ -0,0 +1,8 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(` 😭 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
