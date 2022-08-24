ac05a889fec487e7fb3a064661a05e845bc519f5
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-24 10:28:54 -0600

    added countonly file

---
+++ countOnly.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(` 😭 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
