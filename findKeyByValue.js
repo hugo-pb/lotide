412566c1fdf4f5d0566d0a04bb529d914b75b5b0
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-24 15:53:28 -0600

    findkeybyValue Answer and test

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,3 +6,21 @@ const assertEqual = function(actual, expected) {
     console.log(` 😭 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const findKeyByValue = (obj, tvshow) => {
+  for (i in obj){
+    if(obj[i] === tvshow){
+      return i;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
