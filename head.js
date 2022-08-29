6cd42e10df531e682679b1fb2bf27dacd430d144
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-28 22:39:58 -0600

    import and use asserequal to head.js

--- head.js
+++ head.js
@@ -1,12 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😎 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(` 😭 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+assertEqual = require("./assertEqual");
 const head = function(arr) {
   return arr[0];
 };
