88e45d505d6c04bcda1a21ffbbf7c3712b036917
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-29 10:32:17 -0600

    head exported and own test file added

--- head.js
+++ head.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = require("./assertEqual");
+
 const head = function(arr) {
   return arr[0];
 };
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
