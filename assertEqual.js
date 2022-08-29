ce7b46ae94072564f79604d7f4bdaa533b5237fa
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-28 22:34:53 -0600

    exporting assert Equal

--- assertEqual.js
+++ assertEqual.js
@@ -3,10 +3,12 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😎 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(` 😭 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`😭 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+module.exports = assertEqual;
