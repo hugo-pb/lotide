35e687ea22ae79bdec015a27cc515c27ceb783e3
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-29 16:59:13 -0600

    added index.js to add all functions togerther to export

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,10 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const assert = require("chai").assert;
+
+describe("#middle", () => {
+  it("returns Lighthouse, Labs for Hello, Lighthouse, Labs", () => {
+    ans = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    assert.deepStrictEqual(ans, ["Lighthouse", "Labs"]);
+  });
+});
