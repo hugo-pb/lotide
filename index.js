40c3d761955c8199def592a82e20cc135d75957a
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-29 17:11:15 -0600

    added eqArr to index

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const tail = require("./tail");
 const middle = require("./middle");
 const countLetters = require("./countLetters");
 const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
   countLetters: countLetters,
-  countOnly: countOnly
+  countOnly: countOnly,
+  eqArrays: eqArrays
 };
