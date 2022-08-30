47af10c8f9cf04a0e65f810e3c217f66c4aadae8
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-29 20:30:08 -0600

    added more test files and fixed error on tail.test.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,7 +2,7 @@ const tail = require("../tail");
 
 const assert = require("chai").assert;
 
-describe("#middle", () => {
+describe("#tail", () => {
   it("returns Lighthouse, Labs for Hello, Lighthouse, Labs", () => {
     ans = tail(["Yo Yo", "Lighthouse", "Labs"]);
     assert.deepStrictEqual(ans, ["Lighthouse", "Labs"]);
