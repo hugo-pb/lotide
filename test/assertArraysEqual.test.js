80fa5df032971739dffb890658557ab178dae8c7
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-29 20:30:55 -0600

    comment out assert data

--- test/assertArraysEqual.test.js
+++ test/assertArraysEqual.test.js
@@ -1,2 +1,2 @@
-const assertArraysEqual = require("../assertArraysEqual");
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// const assertArraysEqual = require("../assertArraysEqual");
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
